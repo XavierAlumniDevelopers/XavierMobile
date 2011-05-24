7dcb3c0a39ce2ed1998d04a0cde544b80ae08b8d
Jayson Ng <jayson@rxwebservices.com>
2011-05-25 02:29:24 +0800

    updated feed url source to fix invalid feed direct from w4.xs.edu.ph
    Signed-off-by: Jayson Ng <jayson@rxwebservices.com>

--- Resources/app.js
+++ Resources/app.js
@@ -146,7 +146,9 @@ refreshButton.addEventListener('click',function()
 });
 w4Window.add(refreshButton)
 
-var url = 'http://developer.appcelerator.com/blog/feed';
+var url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=1dd14b3cfa6df4e718b7dee82118c78a&_render=rss';
+// var url = 'http://xavierstallion.tk/';
+
 var data;
 
 function displayItems(itemList){
