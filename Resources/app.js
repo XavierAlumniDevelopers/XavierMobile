dd8c8fe957e10684999fe9f95c4c9ce8b9f1e25a
Jayson Ng <jayson@rxwebservices.com>
2011-06-03 11:14:15 +0800

    moved around some code. functions all at the bottom.
    Signed-off-by: Jayson Ng <jayson@rxwebservices.com>

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,258 @@
+var data;
+
+var url = 'http://developer.appcelerator.com/blog/feed';
+	//var url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=1dd14b3cfa6df4e718b7dee82118c78a&_render=rss';
+
+var win = Titanium.UI.createWindow({
+	title:"Window",
+	backgroundImage:"bkg3.png"
+});
+
+var tabGroup = Ti.UI.createTabGroup();
+
+var mainTabWindow = Titanium.UI.createWindow({
+	title:"Xavier Mobile",
+	navBarHidden:true
+});
+
+mainTabWindow.add(tabGroup)
+
+
+
+
+
+
+
+
+
+var nav = Titanium.UI.iPhone.createNavigationGroup({
+   window: mainTabWindow
+});
+
+// TO DO => SEARCH
+
+/*var searchBar = Ti.UI.createSearchBar({
+	height: 40,
+	top: 0
+});
+
+var searchWin = Ti.UI.createWindow({});
+
+searchBar.addEventListener('return', function(e) {
+	searchWin.title = "Searching for: " + searchBar.value;
+	
+	nav.open(searchWin);
+});*/
+
+var w4Window = Ti.UI.createWindow({
+	backgroundImage:'bkg3.png',
+	title:'Xavier Mobile'
+   	// backButtonTitle:'Back',
+   	// navBarHidden:true
+});
+
+var w4PreviewWindow = Ti.UI.createWindow({
+	backgroundImage:'bkg3.png',
+   	backButtonTitle:'Back',
+   	title:"Preview",
+   	navBarHidden:false
+});
+
+var w4WebViewWindow = Ti.UI.createWindow({
+	backgroundImage:'bkg3.png',
+   	backButtonTitle:'Back',
+   	navBarHidden:false
+});
+
+var w4WindowTab = Ti.UI.createTab({
+	title:"News",
+	icon:'news.png',
+	window: w4Window
+});
+
+tabGroup.addTab(w4WindowTab);
+
+/*
+ * ABOUT WINDOW
+ */
+
+var aboutWindow = Titanium.UI.createWindow({
+    title:"XA::Developers",
+    // navBarHidden:true
+});
+
+var aboutWindowTab = Ti.UI.createTab({
+	title:"About",
+	icon:'source-code-icon.png',
+	// navBarHidden:true,
+	window:aboutWindow
+});
+
+tabGroup.addTab(aboutWindowTab);
+
+var aboutView = Ti.UI.createView({
+   	backgroundColor:'#fff',
+ 	borderColor:'#ddd',
+ 	borderWidth:1,
+    borderRadius:10,
+    width:"90%",
+    height:"80%"
+});
+
+
+var developersLabel = Ti.UI.createLabel({
+	color:'#999',
+	text:'This app is developed for the Xavier community by:',
+	font:{fontSize:14,fontFamily:'Helvetica Neue',fontStyle:'bold'},
+	width:'auto',
+    height: 'auto',
+    top: 15,
+    textAlign:'center',
+
+});
+
+var developersUrlLabel = Ti.UI.createLabel({
+	color:'#999',
+	text:'http://developers.xaverians.com/',
+	font:{fontSize:14,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto',
+	height:'auto',
+    bottom: 20
+});
+
+var xadWindow = Ti.UI.createWindow({
+	title:"XavierAlumniDevelopers",
+	backgroundImage:'bkg3.png',
+	backButtonTitle:'Back',
+	navBarHidden:false
+});
+
+
+developersUrlLabel.addEventListener('click', function(e) {
+
+	nav.open(xadWindow,{animated:true});
+	var xadWebView = Ti.UI.createWebView({
+	    url:'http://developers.xaverians.com/'
+	});
+
+	xadWindow.add(xadWebView);
+
+});
+
+var devLogo = Ti.UI.createImageView({
+    image:'xsdev-logo.png',
+    top:60,
+    width:180,
+    height:180
+});
+
+devLogo.addEventListener('click', function(e) {
+
+	nav.open(xadWindow,{animated:true});
+	var xadWebView = Ti.UI.createWebView({
+	    url:'http://developers.xaverians.com/'
+	});
+
+	xadWebView.title('XA::Developers');
+	xadWindow.add(xadWebView);
+
+});
+
+aboutView.add(developersLabel);
+aboutView.add(devLogo);
+aboutView.add(developersUrlLabel);
+
+aboutWindow.add(aboutView);
+
+/*aboutButton.addEventListener('click', function(e) {
+	nav.open(aboutWindow,{animated:true});
+});*/
+
+/*
+ * ABOUT WINDOW END
+ */
+
+
+/*
+ * NEW RSS based on KitchenSink
+ */
+
+var refreshButton = Titanium.UI.createButton({
+	systemButton:Titanium.UI.iPhone.SystemButton.REFRESH
+	//title:"Refresh",
+	//width:340,
+	//height:50,
+	//top:320
+});
+refreshButton.addEventListener('click',function()
+{
+	// reload feed
+	loadRSSFeed(url);	
+});
+//w4Window.add(refreshButton)
+
+
+
+//Initial loading of the RSS
+loadRSSFeed(url);
+
+/*
+ * END NEW RSS
+ */
+
+/*var aboutButton = Ti.UI.createButton({
+	title: "About"
+});*/
+
+w4Window.setRightNavButton(refreshButton);
+//w4Window.add(searchBar);
+win.add(nav);
+win.open();
+
+/*
+ * ABOUT XS WINDOW
+ */
+/*
+var aboutXSWindow = Ti.UI.createWindow({
+	title:"About XS",
+	// navBarHidden:true
+});
+
+var aboutXSWindowTab = Ti.UI.createTab({
+	title:"XS",
+	icon:'i-icon.png',
+	// navBarHidden:true,
+	window:aboutXSWindow
+});
+
+tabGroup.addTab(aboutXSWindowTab);
+
+var dummyXSAboutLabel = Ti.UI.createLabel({
+	color:'#999',
+	text:'Insert "About XS" here.',
+	font:{fontSize:14,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+aboutXSWindow.add(dummyXSAboutLabel);
+*/
+/*
+ * END ABOUT WINDOW
+ */
+
+
+/*mainWindow.addEventListener('click', function(e) {
+	searchBar.blur();
+});*/
+
+tabGroup.open();
+
+
+
+
+
 function strip_tags (str, allowed_tags) {
     // http://kevin.vanzonneveld.net
     // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
@@ -26,9 +281,6 @@ function strip_tags (str, allowed_tags) {
     // *     returns 3: '<a href='http://kevin.vanzonneveld.net'>Kevin van Zonneveld</a>'
     // *     example 4: strip_tags('1 < 5 5 > 1');
     // *     returns 4: '1 < 5 5 > 1'
-
-	var url = 'http://developer.appcelerator.com/blog/feed';
-//var url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=1dd14b3cfa6df4e718b7dee82118c78a&_render=rss';
 
     var key = '', allowed = false;
     var matches = [];
@@ -95,164 +347,7 @@ function strip_tags (str, allowed_tags) {
     return str;
 }
 
-var win = Titanium.UI.createWindow({
-	title:"Window",
-	backgroundImage:"bkg3.png"
-});
 
-var tabGroup = Ti.UI.createTabGroup();
-
-var mainTabWindow = Titanium.UI.createWindow({
-	title:"Xavier Mobile",
-	navBarHidden:true
-});
-
-mainTabWindow.add(tabGroup)
-
-var nav = Titanium.UI.iPhone.createNavigationGroup({
-   window: mainTabWindow
-});
-
-// TO DO => SEARCH
-
-/*var searchBar = Ti.UI.createSearchBar({
-	height: 40,
-	top: 0
-});
-
-var searchWin = Ti.UI.createWindow({});
-
-searchBar.addEventListener('return', function(e) {
-	searchWin.title = "Searching for: " + searchBar.value;
-	
-	nav.open(searchWin);
-});*/
-
-var w4Window = Ti.UI.createWindow({
-	backgroundImage:'bkg3.png',
-	title:'Xavier Mobile'
-   	// backButtonTitle:'Back',
-   	// navBarHidden:true
-});
-
-var w4PreviewWindow = Ti.UI.createWindow({
-	backgroundImage:'bkg3.png',
-   	backButtonTitle:'Back',
-   	title:"Preview",
-   	navBarHidden:false
-});
-
-var w4WebViewWindow = Ti.UI.createWindow({
-	backgroundImage:'bkg3.png',
-   	backButtonTitle:'Back',
-   	navBarHidden:false
-});
-
-var w4WindowTab = Ti.UI.createTab({
-	title:"News",
-	icon:'news.png',
-	window: w4Window
-});
-
-tabGroup.addTab(w4WindowTab);
-
-/* 
- * OLD RSS BASED ON http://query7.com/titanium-mobile-android-development-first-application
- */
-
-/*
-// create table view
-var rssData = [];
-var w4RSS = Titanium.UI.createTableView({
-	data:rssData,
-	backgroundImage:'bkg3.png'
-});
-
-w4Window.add(w4RSS);
-
-var xhr = Titanium.Network.createHTTPClient();
-
-xhr.onload = function()
-{
-
-	try
-	{
-		
-		var doc = this.responseXML.documentElement;
-		var items = doc.getElementsByTagName('item');
-		var doctitle = doc.evaluate("//channel/title/text()").item(0).nodeValue;
-
-		var urls = new Array();
-		var row = new Array();
-		
-		for(var c=0; c<items.length;c++)
-		{
-			urls[c] = items.item(c).getElementsByTagName('link').item(0).text;
-
-			var postName = items.item(c).getElementsByTagName('title').item(0).text;
-			var postUrl = items.item(c).getElementsByTagName('link').item(0).text;
-
-			row[c] = Titanium.UI.createTableViewRow({
-				title: postName,
-				postName: postName,
-				postUrl: postUrl
-			});
-
-			if(c == 0)
-			{
-				row[c].header = 'Xavier Website Feed';
-			}
-
-			row[c].addEventListener('click', function (e){
-				
-				nav.open(w4WebViewWindow,{animated:true});
-					w4WebViewWindow.title = e.source.postName;
-					var w4WebView = Ti.UI.createWebView({
-	 				   url:e.source.postUrl
-					});
-
-				w4WebViewWindow.add(w4WebView);
-				
-			});
-
-			w4RSS.appendRow(row[c]);
-			
-		}
-
-	}
-	catch(E)
-	{
-		alert("An error has occured\n\nPlease send an Email to xad@xaverians.com with the error below:\n \n" + E);
-	}
-
-};
-
-xhr.open('GET', 'http://developer.appcelerator.com/blog/feed');
-xhr.send();
-*/
-/*
- * END OLD RSS
- */
-
-/*
- * NEW RSS based on KitchenSink
- */
-
-var refreshButton = Titanium.UI.createButton({
-	systemButton:Titanium.UI.iPhone.SystemButton.REFRESH
-	//title:"Refresh",
-	//width:340,
-	//height:50,
-	//top:320
-});
-refreshButton.addEventListener('click',function()
-{
-	// reload feed
-	loadRSSFeed(url);	
-});
-//w4Window.add(refreshButton)
-
-var data;
 
 function displayItems(itemList){
 
@@ -452,152 +547,3 @@ function loadRSSFeed(url){
 	xhr.send();	
 	
 }
-
-//Initial loading of the RSS
-loadRSSFeed(url);
-
-/*
- * END NEW RSS
- */
-
-/*var aboutButton = Ti.UI.createButton({
-	title: "About"
-});*/
-
-w4Window.setRightNavButton(refreshButton);
-//w4Window.add(searchBar);
-win.add(nav);
-win.open();
-
-/*
- * ABOUT XS WINDOW
- */
-/*
-var aboutXSWindow = Ti.UI.createWindow({
-	title:"About XS",
-	// navBarHidden:true
-});
-
-var aboutXSWindowTab = Ti.UI.createTab({
-	title:"XS",
-	icon:'i-icon.png',
-	// navBarHidden:true,
-	window:aboutXSWindow
-});
-
-tabGroup.addTab(aboutXSWindowTab);
-
-var dummyXSAboutLabel = Ti.UI.createLabel({
-	color:'#999',
-	text:'Insert "About XS" here.',
-	font:{fontSize:14,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-aboutXSWindow.add(dummyXSAboutLabel);
-*/
-/*
- * END ABOUT WINDOW
- */
-
-/*
- * ABOUT WINDOW
- */
-
-var aboutWindow = Titanium.UI.createWindow({
-    title:"About",
-    // navBarHidden:true
-});
-
-var aboutWindowTab = Ti.UI.createTab({
-	title:"About",
-	icon:'source-code-icon.png',
-	// navBarHidden:true,
-	window:aboutWindow
-});
-
-tabGroup.addTab(aboutWindowTab);
-
-var aboutView = Ti.UI.createView({
-    backgroundColor:'#fff',
-    borderColor:'#ddd',
-    borderWidth:1,
-    borderRadius:10,
-    width:"90%",
-    height:"80%"
-});
-
-var devLogo = Ti.UI.createImageView({
-    image:'xsdev-logo.png',
-    top:50,
-    width:100,
-    height:100
-});
-
-var developersLabel = Ti.UI.createLabel({
-	color:'#999',
-	text:'Developed by:',
-	font:{fontSize:18,fontFamily:'Helvetica Neue',fontStyle:'bold'},
-	textAlign:'center',
-	width:'auto',
-    top: 50
-});
-
-var developer1Label = Ti.UI.createLabel({
-	color:'#999',
-	text:'Xavier Alumni Developers\nhttp://developers.xaverians.com/',
-	font:{fontSize:14,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto',
-    top: 120
-});
-
-var xadWindow = Ti.UI.createWindow({
-	title:"XavierAlumniDevelopers",
-	backgroundImage:'bkg3.png',
-	backButtonTitle:'Back',
-	navBarHidden:false
-});
-
-
-developer1Label.addEventListener('click', function(e) {
-
-	nav.open(xadWindow,{animated:true});
-	var xadWebView = Ti.UI.createWebView({
-	    url:'http://developers.xaverians.com/'
-	});
-
-	xadWindow.add(xadWebView);
-
-});
-
-devLogo.addEventListener('click', function(e) {
-
-	nav.open(xadWindow,{animated:true});
-	var xadWebView = Ti.UI.createWebView({
-	    url:'http://developers.xaverians.com/'
-	});
-
-	xadWindow.add(xadWebView);
-
-});
-
-aboutView.add(devLogo);
-aboutView.add(developersLabel);
-aboutView.add(developer1Label);
-aboutWindow.add(aboutView);
-
-/*aboutButton.addEventListener('click', function(e) {
-	nav.open(aboutWindow,{animated:true});
-});*/
-
-/*
- * ABOUT WINDOW END
- */
-
-/*mainWindow.addEventListener('click', function(e) {
-	searchBar.blur();
-});*/
-
-tabGroup.open();
